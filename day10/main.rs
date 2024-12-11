165b43514a97981534077183b692c30b16c652da
Dobby <thomas.dobbelaere@uclouvain.be>
2024-12-11 17:32:20 +0100

    simplify day 10

--- day10/main.rs
+++ day10/main.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::HashSet;
 
-fn find_trail_part1(pos: &(i32,i32), value: i32, topo: &Vec<u8>, imax: i32, jmax: i32, tails: &mut Vec<(i32,i32)>) {
-    if value == 9 && !tails.contains(pos) {
+fn find_trails(pos: &(i32,i32), value: i32, topo: &Vec<u8>, imax: i32, jmax: i32, tails: &mut Vec<(i32,i32)>) {
+    if value == 9 {
         tails.push(*pos);
     }
     for (ii,jj) in [(-1,0), (1,0), (0,-1), (0,1)].iter() {
@@ -13,30 +14,11 @@ fn find_trail_part1(pos: &(i32,i32), value: i32, topo: &Vec<u8>, imax: i32, jmax
         }
         let newvalue = topo[(i*jmax+j) as usize] as i32;
         if newvalue - value == 1 {
-            find_trail_part1(&(i,j), newvalue, topo, imax, jmax, tails);
+            find_trails(&(i,j), newvalue, topo, imax, jmax, tails);
         }
     }
 }
 
-
-fn count_trails_part2(pos: &(i32,i32), value: i32, topo: &Vec<u8>, imax: i32, jmax: i32) -> usize {
-    if value == 9  {
-        return 1;
-    }
-    let mut score: usize = 0;
-    for (ii,jj) in [(-1,0), (1,0), (0,-1), (0,1)].iter() {
-        let i = pos.0+*ii as i32;
-        let j = pos.1+*jj as i32;
-        if i < 0 || i >= imax || j < 0 || j >= jmax {
-            continue;
-        }
-        let newvalue = topo[(i*jmax+j) as usize] as i32;
-        if newvalue - value == 1 {
-            score += count_trails_part2(&(i,j), newvalue, topo, imax, jmax);
-        }
-    }
-    score
-}
 
 fn main() {
     let file = File::open("input").unwrap();
@@ -62,9 +44,9 @@ fn main() {
     let mut totalscore2: usize = 0;
     for seed in seeds.iter() {
         let mut tails = Vec::<(i32,i32)>::new();
-        find_trail_part1(seed, 0, &topo, imax, jmax, &mut tails);
-        totalscore1 += tails.len();
-        totalscore2 += count_trails_part2(seed, 0, &topo, imax, jmax);
+        find_trails(seed, 0, &topo, imax, jmax, &mut tails);
+        totalscore2 += tails.len();
+        totalscore1 += tails.iter().collect::<HashSet<_>>().len();
     }
     println!("Total score part 1: {}", totalscore1);
     println!("Total score part 2: {}", totalscore2);
