ab1e8e280a6f590f7c5f1b5459c206333c8270c6
Dobby <thomas.dobbelaere@uclouvain.be>
2024-12-16 17:04:17 +0100

    changed a bit but still not working...

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -11,6 +11,18 @@ enum Direction {
     Down
 }
 
+struct MazeCell {
+    x: i32,
+    y: i32,
+    direction: Direction,
+    score: i32,
+    visited: Vec<(i32,i32)>,
+}
+
+fn distance_from_end(mc: &MazeCell, end: &(i32,i32)) -> i32 {
+    (mc.x-end.0).abs() + (mc.y-end.1).abs()
+}
+
 fn displacement(direction: &Direction) -> (i32, i32) {
     match direction {
         Direction::Left => (-1, 0),
@@ -38,31 +50,58 @@ fn turn_right(direction: &Direction) -> Direction {
     }
 }
 
-fn solve_maze_dumb(
-    x: i32, y: i32, direction: Direction, score: i32, end: &(i32,i32),
-    walls: &Vec<(i32,i32)>, visited: &mut Vec<(i32,i32)>, found: &mut Vec<i32>
-) -> bool {
-    if x == end.0 && y == end.1 {
-        found.push(score);
-        return true;
+fn get_next_cells(mc: &MazeCell, walls: &Vec<(i32,i32)>) -> Vec<MazeCell> {
+    let left_dir = turn_left(&mc.direction);
+    let right_dir = turn_right(&mc.direction);
+    let mut cells = Vec::<MazeCell>::new();
+    for (i,d) in [mc.direction.clone(), left_dir, right_dir].iter().enumerate() {
+        let (shiftx, shifty) = displacement(d);
+        let (x, y) = (mc.x+shiftx, mc.y+shifty);
+        if walls.contains(&(x,y)) || mc.visited.contains(&(x,y)) {
+            continue;
+        }
+        let mut visited = mc.visited.clone();
+        visited.push((mc.x, mc.y));
+        let mut score = mc.score;
+        if i == 0 {
+            score += 1;
+        } else {
+            score += 1001;
+        }
+        cells.push(MazeCell { x, y, direction: d.clone(), score, visited });
     }
-    if visited.contains(&(x,y)) || walls.contains(&(x,y)){
-        return false;
+    cells
+}
+
+fn solve_maze(candidates: &mut Vec<MazeCell>, closed: &mut Vec<(i32,i32)>, end: &(i32,i32), walls: &Vec<(i32,i32)>) -> i32 {
+    let mut ibest: usize = 0;
+    let mut best_score = i32::MAX;
+    for (i,mc) in candidates.iter().enumerate() {
+        let score = mc.score + distance_from_end(mc, end);
+        if score < best_score {
+            best_score = score;
+            ibest = i;
+        }
     }
-    visited.push((x,y));
-    let (shiftx, shifty) = displacement(&direction);
-    let mut result = false;
-    result |= solve_maze_dumb(x+shiftx, y+shifty, direction.clone(), score+1, end, walls, visited, found);
-    if result {
-        return true;
+    let best = &candidates[ibest];
+    if best.x == end.0 && best.y == end.1 {
+        return best.score;
     }
-    let left_dir = turn_left(&direction);
-    let (shiftx, shifty) = displacement(&left_dir);
-    result |= solve_maze_dumb(x+shiftx, y+shifty, left_dir, score+1001, end, walls, &mut visited.clone(), found);
-    let right_dir = turn_right(&direction);
-    let (shiftx, shifty) = displacement(&right_dir);
-    result |= solve_maze_dumb(x+shiftx, y+shifty, right_dir, score+1001, end, walls, &mut visited.clone(), found);
-    result
+    let new_cells = get_next_cells(best, walls);
+    closed.push((best.x, best.y));
+    for cell in new_cells {
+        candidates.push(cell);
+    }
+    let mut remove = Vec::<usize>::new();
+    for (i,mc) in candidates.iter().enumerate() {
+        if closed.contains(&(mc.x, mc.y)) {
+            remove.push(i);
+        }
+    }
+    for i in remove.iter().rev() {
+        candidates.remove(*i);
+    }
+    return solve_maze(candidates, closed, end, walls)
 }
 
 fn main() {
@@ -71,7 +110,6 @@ fn main() {
     let file = File::open(&args[1]).unwrap();
     let reader = BufReader::new(file);
     let mut walls = Vec::<(i32, i32)>::new();
-    let mut visited = Vec::<(i32, i32)>::new();
     let mut start = (-1, -1);
     let mut end = (-1, -1);
     for (iy,line) in reader.lines().enumerate() {
@@ -86,7 +124,9 @@ fn main() {
             }
         }
     }
-    let mut scores = Vec::<i32>::new();
-    solve_maze_dumb(start.0, start.1, Direction::Right, 0, &end, &walls, &mut visited, &mut scores);
-    println!("Minimum score: {}", scores.iter().min().unwrap());
+    let mut candidates = Vec::<MazeCell>::new();
+    candidates.push(MazeCell { x: start.0, y: start.1, direction: Direction::Right, score: 0, visited: Vec::<(i32,i32)>::new() });
+    let mut closed = Vec::<(i32,i32)>::new();
+    let score = solve_maze(&mut candidates, &mut closed, &end, &walls);
+    println!("Best score: {}", score);
 }
