9e530b1d3b85256428f2bcd33796ec0ff6dfd8d4
Dobby <thomas.dobbelaere@uclouvain.be>
2024-12-19 11:20:38 +0100

    started part 2 (not ok for real input)

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,22 +1,45 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::cmp::{max,min};
+use std::collections::HashSet;
 use std::env;
 
 fn ispossible(design: &str, patterns: &Vec<Vec<&str>>, nmax: &usize) -> bool {
-    if design.len() <= patterns.len() && patterns[design.len()-1].contains(&design) {
+    if design.len() == 0 {
         return true;
     }
-    for n in 1..min(*nmax+1,design.len()) {
-        let part = &design[..n];
-        if !patterns[n-1].contains(&part) {
+    for n in 0..min(*nmax,design.len()) {
+        let part = &design[..n+1];
+        if !patterns[n].contains(&part) {
             continue;
         }
-        if ispossible(&design[n..], patterns, nmax) {
+        if ispossible(&design[n+1..], patterns, nmax) {
             return true;
         }
     }
     false
+}
+
+fn find_all_ways(design: &str, patterns: &Vec<Vec<&str>>, nmax: &usize, prev: String, ways: &mut Vec<String>) {
+    if design.len() == 0 {
+        ways.push(prev);
+        return
+    }
+    for n in 1..min(*nmax+1,design.len()+1) {
+        let part = &design[..n];
+        for towel in patterns[n-1].iter() {
+            if part == *towel {
+                let seq: String;
+                if prev.len() == 0 {
+                    seq = towel.to_string();
+                }
+                else {
+                    seq = format!("{},{}", prev, towel);
+                }
+                find_all_ways(&design[n..], patterns, nmax, seq, ways)
+            }
+        }
+    }
 }
 
 fn ispossible_split(design: &str, patterns: &Vec<Vec<&str>>, nmax: &usize) -> bool {
@@ -52,6 +75,7 @@ fn main() {
         sorted_towels.push(selected);
     }
     let mut n_possible = 0;
+    let mut n_ways = 0;
     while let Some(line) = lines.next() {
         let line = line.unwrap();
         if line.len() == 0 {
@@ -60,6 +84,11 @@ fn main() {
         if ispossible_split(&line, &sorted_towels, &nmax) {
             n_possible += 1;
         }
+        let prev = String::from("");
+        let mut ways = Vec::<String>::new();
+        find_all_ways(&line, &sorted_towels, &nmax, prev, &mut ways);
+        n_ways += ways.iter().collect::<HashSet<_>>().len();
     }
     println!("{} designs are possible", n_possible);
+    println!("{} possible ways", n_ways);
 }
