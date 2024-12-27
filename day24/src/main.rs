8f5bac5bbc748f54eee1f4b08e892cbda6c4649e
Dobby <thomas.dobbelaere@uclouvain.be>
2024-12-27 23:14:54 +0100

    some improvements for day24 part 2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::collections::{HashMap,HashSet};
-use std::cmp::{min,max};
 use std::env;
 
 #[derive(Debug)]
@@ -17,26 +16,41 @@ struct Gate {
     input1: String,
     input2: String,
     output: String,
-    depth: usize,
 }
 
+#[allow(dead_code)]
 fn get_kth_bit(n: u64, k: usize) -> u8 {
     ((n >> k) & 1) as u8
 }
 
+#[allow(dead_code)]
 fn switch_outputs(gates: &mut Vec<Gate>, index1: usize, index2: usize) {
     let tmp = gates[index1].output.clone();
     gates[index1].output = gates[index2].output.clone();
     gates[index2].output = tmp;
 }
 
-fn solve_gates(gates: &mut Vec<Gate>, wire_values: &mut HashMap<String,Option<u8>>) -> bool{
+fn build_connectivity(gates: &Vec<Gate>) -> HashMap<usize,(Vec<usize>, Vec<usize>)>{
+    let mut conn = HashMap::<usize,(Vec<usize>, Vec<usize>)>::new();
+    for i in 0..gates.len() {
+        conn.entry(i).or_insert((Vec::<usize>::new(), Vec::<usize>::new()));
+    }
+    for (i,gate) in gates.iter().enumerate() {
+        for (j,other) in gates.iter().enumerate() {
+            if gate.input1 == other.output || gate.input2 == other.output {
+                conn.entry(i).and_modify(|v| v.0.push(j));
+                conn.entry(j).and_modify(|v| v.1.push(i));
+            }
+        }
+    }
+    conn
+}
+
+fn solve_gates(gates: &Vec<Gate>, wire_values: &mut HashMap<String,Option<u8>>) {
     let n_wires = wire_values.len();
     let mut n_values = wire_values.iter().filter(|(_,v)| v.is_some()).count();
-    let mut depth = 0;
     while n_values < n_wires {
-        let prev = n_values;
-        for gate in gates.iter_mut() {
+        for gate in gates.iter() {
             match wire_values.get(&gate.output).unwrap() {
                 Some(_) => continue,
                 None => (),
@@ -49,7 +63,6 @@ fn solve_gates(gates: &mut Vec<Gate>, wire_values: &mut HashMap<String,Option<u8
                 Some(v) => *v,
                 None => continue,
             };
-            gate.depth = depth;
             wire_values.entry(gate.output.clone()).and_modify(|v| {
                 *v = match gate.operation {
                     Operation::AND => Some(v1 & v2),
@@ -59,31 +72,83 @@ fn solve_gates(gates: &mut Vec<Gate>, wire_values: &mut HashMap<String,Option<u8
             });
             n_values += 1;
         }
-        depth += 1;
-        if n_values == prev {
-            return false;
-        }
-    }
-    true
-}
-
-fn find_upstream_gates(gates: &Vec<Gate>, wire: &String, found: &mut Vec<usize>) {
-    for (i,gate) in gates.iter().enumerate() {
-        if gate.output != *wire {
-            continue;
-        }
-        if found.contains(&i) {
-            continue;
-        }
-        found.push(i);
-        find_upstream_gates(gates, &gate.input1, found);
-        find_upstream_gates(gates, &gate.input2, found);
-        return
     }
 }
 
-fn check_results(wire_values: &HashMap<String,Option<u8>>, expected: u64) -> Vec<String> {
-    let z_wires = wire_values.keys().filter(|k| k.starts_with('z')).map(|v| v.clone()).collect::<Vec<String>>();
+fn recompute_gates(
+    gates: &Vec<Gate>, wire_values: &mut HashMap<String,Option<u8>>,
+    connectivity: &HashMap<usize,(Vec<usize>,Vec<usize>)>, next: &Vec<usize>
+) {
+    for i in next.iter() {
+        let gate = &gates[*i];
+        let v1 = wire_values.get(&gate.input1).unwrap().unwrap();
+        let v2 = wire_values.get(&gate.input2).unwrap().unwrap();
+        wire_values.entry(gate.output.clone()).and_modify(|v| {
+            *v = match gate.operation {
+                Operation::AND => Some(v1 & v2),
+                Operation::OR => Some(v1 | v2),
+                Operation::XOR => Some(v1 ^ v2),
+            };
+        });
+        let c = connectivity.get(i).unwrap();
+        recompute_gates(gates, wire_values, connectivity, &c.1);
+    }
+}
+
+fn find_upstream_gates(
+    gates: &Vec<Gate>, wire: &String, wire_values: &HashMap<String,Option<u8>>,
+    bad_bits: &Vec<String>, connectivity: &HashMap<usize,(Vec<usize>,Vec<usize>)>,
+    to_one: &mut Vec<usize>, to_zero: &mut Vec<usize>
+) {
+    let index = gates.iter().position(|g| g.output == *wire).unwrap();
+    let mut gate_ids = HashSet::<usize>::new();
+    gate_ids.insert(index);
+    let conn = connectivity.get(&index).unwrap();
+    let mut previous = conn.0.clone();
+    while previous.len() > 0 {
+        let mut next = Vec::<usize>::new();
+        for p in previous.iter() {
+            gate_ids.insert(*p);
+            let c = connectivity.get(p).unwrap();
+            next.extend(c.0.clone());
+        }
+        previous = next.clone();
+    }
+    for start_gate in gate_ids.iter() {
+        let mut new_values = wire_values.clone();
+        let output_tag = gates[*start_gate].output.clone();
+        let value = new_values.get(&output_tag).unwrap().unwrap();
+        let iszero = value == 0;
+        new_values.entry(output_tag).and_modify(|v| *v = Some(if iszero {1} else {0}));
+        let next = connectivity.get(start_gate).unwrap();
+        recompute_gates(gates, &mut new_values, connectivity, &next.1);
+        let mut ok = true;
+        let mut changed = 0;
+        for (tag, value) in new_values.iter() {
+            if !tag.starts_with('z') {
+                continue;
+            }
+            if wire_values[tag].unwrap() != value.unwrap() {
+                if !bad_bits.contains(tag) {
+                    ok = false;
+                    break;
+                }
+                changed += 1;
+            }
+        }
+        if !ok || changed == 0 {
+            continue;
+        }
+        if iszero && !to_one.contains(start_gate) {
+            to_one.push(*start_gate);
+        }
+        if !iszero && !to_zero.contains(start_gate){
+            to_zero.push(*start_gate);
+        }
+    }
+}
+
+fn check_results(wire_values: &HashMap<String,Option<u8>>, z_wires: &Vec<String>, expected: u64) -> Vec<String> {
     let mut bad = Vec::<String>::new();
     for (i,wire) in z_wires.iter().enumerate() {
         let value = wire_values.get(wire).unwrap().unwrap();
@@ -95,33 +160,12 @@ fn check_results(wire_values: &HashMap<String,Option<u8>>, expected: u64) -> Vec
     bad
 }
 
-//fn solve_switch(
-//    gates: &mut Vec<Gate>, initial_state: &HashMap<String,Option<u8>>, switches: Vec<(usize,usize)>, expected: u64
-//) -> Option<Vec<(usize,usize)>> {
-//    let mut wire_values = initial_state.clone();
-//    solve_gates(gates, &mut wire_values);
-//    let bad_bits = check_results(&wire_values, expected);
-//    if switches.len() == 4 {
-//        return if bad_bits.len() == 0 { Some(switches) } else { None };
-//    }
-//    let mut bad_wires = HashMap::<usize,usize>::new();
-//    for wire in bad_bits.iter() {
-//        let mut found = Vec::<usize>::new();
-//        find_upstream_gates(gates, wire, &mut found);
-//        for i in found {
-//            bad_wires.entry(i).and_modify(|v| *v += 1).or_insert(1);
-//        }
-//    }
-//    None
-//}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file = File::open(&args[1]).unwrap();
     let reader = BufReader::new(file);
     let mut wire_values = HashMap::<String,Option<u8>>::new();
     let mut gates = Vec::<Gate>::new();
-    let mut n_values = 0;
     let mut zwires = Vec::<String>::new();
     let mut x: u64 = 0;
     let mut ix = 0;
@@ -137,7 +181,6 @@ fn main() {
             let name = parts[0];
             let value = parts[1].parse::<u8>().unwrap();
             wire_values.insert(name.to_string(), Some(value));
-            n_values += 1;
             if name.starts_with('x') {
                 x += (value as u64) << ix;
                 ix += 1;
@@ -160,7 +203,6 @@ fn main() {
                 input1: parts[0].to_string(),
                 input2: parts[2].to_string(),
                 output: parts[4].to_string(),
-                depth: 0,
             };
             let wires = vec![gate.input1.clone(), gate.input2.clone(), gate.output.clone()];
             for wire in wires {
@@ -172,52 +214,28 @@ fn main() {
             gates.push(gate);
         }
     }
-    zwires.sort();
-    let initial_state = wire_values.clone();
-    solve_gates(&mut gates, &mut wire_values);
+    let connectivity = build_connectivity(&gates);
+    let mut z_wires = wire_values.keys().filter(|k| k.starts_with('z')).map(|v| v.clone()).collect::<Vec<String>>();
+    z_wires.sort();
+    solve_gates(&gates, &mut wire_values);
     let expected = x + y;
-    let mut result = 0;
-    for (i,wire) in zwires.iter().enumerate() {
-        let value = wire_values.get(wire).unwrap().unwrap();
+    let mut result: u64 = 0;
+    for (i,wire) in z_wires.iter().enumerate() {
+        let value = match wire_values.get(wire).unwrap() {
+            Some(v) => *v,
+            None => panic!("Wire {} not solved", wire),
+        };
         result += (value as u64) << i;
     }
     println!("Result: {}", result);
     // Part 2
-    //let bad_bits = check_results(&wire_values, expected);
-    //println!("Bad bits: {:?}", bad_bits);
-    //let mut bad_gates = HashMap::<usize,usize>::new();
-    //for wire in bad_bits.iter() {
-    //    let mut found = Vec::<usize>::new();
-    //    find_upstream_gates(&gates, wire, &mut found);
-    //    for i in found {
-    //        bad_gates.entry(i).and_modify(|v| *v += 1).or_insert(1);
-    //    }
-    //}
-    //let max_count = *bad_gates.values().min().unwrap();
-    //let indx_gate = bad_gates.iter().filter(|(_,v)| **v == max_count).map(|(k,_)| *k).collect::<Vec<usize>>();
-    //println!("{}: {:?}", max_count, indx_gate);
-    //let mut best: i32 = 0;
-    //let mut best_switch = (0,0);
-    //for i in 0..indx_gate.len()-1 {
-    //    for j in i+1..indx_gate.len() {
-    //        let gate1 = indx_gate[i];
-    //        let gate2 = indx_gate[j];
-    //        switch_outputs(&mut gates, indx_gate[i], indx_gate[j]);
-    //        let mut wire_values = initial_state.clone();
-    //        let solved = solve_gates(&mut gates, &mut wire_values);
-    //        if !solved {
-    //            switch_outputs(&mut gates, indx_gate[i], indx_gate[j]);
-    //            continue;
-    //        }
-    //        let new_bad = check_results(&wire_values, expected).len();
-    //        let save = bad_bits.len() as i32 - new_bad as i32;
-    //        println!("{}", save);
-    //        if save > best {
-    //            best = save;
-    //            best_switch = (min(gate1,gate2), max(gate1,gate2));
-    //        }
-    //        switch_outputs(&mut gates, indx_gate[i], indx_gate[j]);
-    //    }
-    //}
-    //println!("Best switch: {:?}", best_switch);
+    let bad_bits = check_results(&wire_values, &z_wires, expected);
+    let mut to_one = Vec::<usize>::new();
+    let mut to_zero = Vec::<usize>::new();
+    for wire in bad_bits.iter() {
+        find_upstream_gates(&gates, wire, &mut wire_values, &bad_bits, &connectivity, &mut to_one, &mut to_zero);
+    }
+    println!("{}", to_one.len());
+    println!("{}", to_zero.len());
+    println!("{}", gates.len());
 }
