bbda5a48607e45ddc6dcde11fd564828d14d5407
Dobby <thomas.dobbelaere@uclouvain.be>
2025-08-09 21:38:41 +0200

    faster code in Rust

--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -3,17 +3,6 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn exec_line(line: &str) -> usize {
-    let mut result = 0;
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    for cap in re.captures_iter(line) {
-        let x = cap.get(1).unwrap().as_str().parse::<usize>().unwrap();
-        let y = cap.get(2).unwrap().as_str().parse::<usize>().unwrap();
-        result += x * y;
-    }
-    result
-}
-
 fn main() {
     let args = env::args().nth(1).expect("Please provide an input file");
     let file = File::open(args).unwrap();
@@ -21,12 +10,19 @@ fn main() {
     let mut part1 = 0;
     let mut part2 = 0;
     let mut active = true;
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     for line in reader.lines() {
         let line = line.unwrap();
         let split: Vec<&str> = line.split("()").collect();
         for entry in split.iter() {
-            if active {
-                part2 += exec_line(entry);
+            for cap in re.captures_iter(entry) {
+                let x = cap.get(1).unwrap().as_str().parse::<usize>().unwrap();
+                let y = cap.get(2).unwrap().as_str().parse::<usize>().unwrap();
+                let product = x * y;
+                part1 += product;
+                if active {
+                    part2 += product;
+                }
             }
             if entry.ends_with("don't") {
                 active = false;
@@ -34,7 +30,6 @@ fn main() {
                 active = true;
             }
         }
-        part1 += exec_line(&line);
     }
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
