36175672326a9b749752d6f6e66a69ff07a07de4
Dobby <thomas.dobbelaere@uclouvain.be>
2025-08-18 14:37:01 +0200

    Update 2024 day 13 in Rust

---
+++ 2024/day13/src/main.rs
@@ -0,0 +1,67 @@
+use anyhow::Result;
+use regex::Regex;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn solve_system(xa: i64, ya: i64, xb: i64, yb: i64, x: i64, y: i64) -> i64 {
+    let det = xa * yb - ya * xb;
+    if det == 0 {
+        return 0;
+    }
+    let na = (x * yb - y * xb) / det;
+    let nb = (xa * y - ya * x) / det;
+    if na < 0 || nb < 0 || na * xa + nb * xb != x || na * ya + nb * yb != y {
+        return 0;
+    }
+    3 * na + nb
+}
+
+fn main() -> Result<()> {
+    let filename = env::args().nth(1).expect("Missing input file");
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+    let mut xa = 0;
+    let mut ya = 0;
+    let mut xb = 0;
+    let mut yb = 0;
+    let pattern = Regex::new(r"([a-zA-z ]+): X[\+=]([0-9]+), Y[\+=]([0-9]+)")?;
+    let mut part1 = 0;
+    let mut part2 = 0;
+    for line in reader.lines() {
+        let line = line?;
+        if line.is_empty() {
+            continue;
+        }
+        let cap = pattern.captures(&line).unwrap();
+        let key = cap.get(1).unwrap().as_str();
+        let x_value = cap.get(2).unwrap().as_str().parse::<i64>()?;
+        let y_value = cap.get(3).unwrap().as_str().parse::<i64>()?;
+        match key {
+            "Button A" => {
+                xa = x_value;
+                ya = y_value;
+            }
+            "Button B" => {
+                xb = x_value;
+                yb = y_value;
+            }
+            "Prize" => {
+                part1 += solve_system(xa, ya, xb, yb, x_value, y_value);
+                part2 += solve_system(
+                    xa,
+                    ya,
+                    xb,
+                    yb,
+                    x_value + 10000000000000,
+                    y_value + 10000000000000,
+                );
+            }
+            _ => eprintln!("Could not parse line '{}'", line),
+        }
+    }
+
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
+    Ok(())
+}
