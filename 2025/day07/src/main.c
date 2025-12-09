6b8f633114ed8b92dc127af89a6b4059df1c5624
Dobby <thomas.dobbelaere@uclouvain.be>
2025-12-09 20:53:33 +0100

    piano piano

--- 2025/day07/src/main.c
+++ 2025/day07/src/main.c
@@ -4,6 +4,22 @@
 
 #define BUFFER_SIZE 1024
 
+struct Splitter {
+  int x;
+  int y;
+  int n_children;
+  struct Splitter *chidren[2];
+};
+
+struct Splitter *create_splitter(int x, int y) {
+  struct Splitter *splitter =
+      (struct Splitter *)malloc(sizeof(struct Splitter));
+  splitter->x = x;
+  splitter->y = y;
+  splitter->n_children = 0;
+  return splitter;
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(stderr, "Missing input file\n");
@@ -17,18 +33,23 @@ int main(int argc, char *argv[]) {
   }
 
   char buffer[BUFFER_SIZE];
+  int startX, startY;
   int nx;
   int ny = 0;
   int capacity = BUFFER_SIZE;
   int size = 0;
-  int startX, startY;
   char *content = (char *)malloc(capacity);
+  int n_splitter = 0;
+  struct Splitter **splitters =
+      (struct Splitter **)malloc(BUFFER_SIZE * sizeof(struct Splitter *));
   while (fgets(buffer, BUFFER_SIZE, fp)) {
     nx = strcspn(buffer, "\n");
     for (int i = 0; i < nx; ++i) {
       if (buffer[i] == 'S') {
         startX = i;
         startY = ny;
+      } else if (buffer[i] == '^') {
+        splitters[n_splitter++] = create_splitter(i, ny);
       }
       if (size >= capacity) {
         capacity *= 2;
@@ -40,6 +61,8 @@ int main(int argc, char *argv[]) {
   }
   fclose(fp);
   content = (char *)realloc(content, size);
+  splitters = (struct Splitter **)realloc(
+      splitters, n_splitter * sizeof(struct Splitter *));
 
   // printf("%d %d %d", nx, ny, size);
   //
@@ -51,6 +74,11 @@ int main(int argc, char *argv[]) {
 
   free(content);
   free(manifold);
+  for (int i = 0; i < n_splitter; i++) {
+    printf("%d, %d\n", splitters[i]->x, splitters[i]->y);
+    free(splitters[i]);
+  }
+  free(splitters);
 
   return EXIT_SUCCESS;
 }
