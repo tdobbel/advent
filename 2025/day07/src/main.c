366d1be56c59f479de80e6feea27d5aecf660147
tdobbel <thomas.dobbelaere@uclouvain.be>
2025-12-19 11:21:35 +0100

    C is fun

--- 2025/day07/src/main.c
+++ 2025/day07/src/main.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,7 +8,9 @@
 struct Splitter {
   int x;
   int y;
+  uint64_t counter;
   int n_children;
+  int touched;
   struct Splitter *chidren[2];
 };
 
@@ -17,7 +20,38 @@ struct Splitter *create_splitter(int x, int y) {
   splitter->x = x;
   splitter->y = y;
   splitter->n_children = 0;
+  splitter->touched = 0;
+  splitter->counter = 0;
+  splitter->chidren[0] = NULL;
+  splitter->chidren[1] = NULL;
   return splitter;
+}
+
+void find_splits(struct Splitter **splitters, int isrc, int n_splitter, int nx,
+                 int left) {
+  struct Splitter *source = splitters[isrc];
+  source->touched = 1;
+  int x = source->x;
+  if (left) {
+    if (x == 0)
+      return;
+    x -= 1;
+  } else {
+    if (x == nx - 1)
+      return;
+    x += 1;
+  }
+  for (int i = isrc + 1; i < n_splitter; ++i) {
+    struct Splitter *splitter = splitters[i];
+    if (splitter->x == x) {
+      source->chidren[source->n_children++] = splitter;
+      if (!splitter->touched) {
+        find_splits(splitters, i, n_splitter, nx, 0);
+        find_splits(splitters, i, n_splitter, nx, 1);
+      }
+      return;
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -36,10 +70,8 @@ int main(int argc, char *argv[]) {
   int startX, startY;
   int nx;
   int ny = 0;
-  int capacity = BUFFER_SIZE;
-  int size = 0;
-  char *content = (char *)malloc(capacity);
   int n_splitter = 0;
+  int capacity = BUFFER_SIZE;
   struct Splitter **splitters =
       (struct Splitter **)malloc(BUFFER_SIZE * sizeof(struct Splitter *));
   while (fgets(buffer, BUFFER_SIZE, fp)) {
@@ -49,33 +81,47 @@ int main(int argc, char *argv[]) {
         startX = i;
         startY = ny;
       } else if (buffer[i] == '^') {
+        if (n_splitter == capacity) {
+          capacity *= 2;
+          splitters = (struct Splitter **)realloc(
+              splitters, capacity * sizeof(struct Splitter *));
+        }
         splitters[n_splitter++] = create_splitter(i, ny);
       }
-      if (size >= capacity) {
-        capacity *= 2;
-        content = (char *)realloc(content, capacity);
-      }
-      content[size++] = buffer[i];
     }
     ny++;
   }
   fclose(fp);
-  content = (char *)realloc(content, size);
   splitters = (struct Splitter **)realloc(
       splitters, n_splitter * sizeof(struct Splitter *));
 
-  // printf("%d %d %d", nx, ny, size);
-  //
-  char **manifold = (char **)malloc(ny * sizeof(char *));
-  manifold[0] = content;
-  for (int i = 1; i < ny; ++i) {
-    manifold[i] = manifold[i - 1] + nx;
+  find_splits(splitters, 0, n_splitter, nx, 0);
+  find_splits(splitters, 0, n_splitter, nx, 1);
+  int part1 = 0;
+  for (int i = 0; i < n_splitter; i++) {
+    if (splitters[i]->touched)
+      part1++;
   }
 
-  free(content);
-  free(manifold);
+  printf("Part 1: %d\n", part1);
+
+  for (int i = n_splitter - 1; i >= 0; --i) {
+    struct Splitter *splitter = splitters[i];
+    if (!splitter->touched)
+      continue;
+    for (int ic = 0; ic < 2; ic++) {
+      struct Splitter *child = splitter->chidren[ic];
+      if (child) {
+        splitter->counter += child->counter;
+      } else {
+        splitter->counter++;
+      }
+    }
+  }
+
+  printf("Part 2: %lu\n", splitters[0]->counter);
+
   for (int i = 0; i < n_splitter; i++) {
-    printf("%d, %d\n", splitters[i]->x, splitters[i]->y);
     free(splitters[i]);
   }
   free(splitters);
