b468773e754c3f6a1f6afbfce3b1033a9c3027ba
Dobby <thomas.dobbelaere@uclouvain.be>
2026-03-21 22:35:39 +0100

    Just had to increase number of buckets to make it faster for part 2...

--- 2020/day15/main.c
+++ 2020/day15/main.c
@@ -14,7 +14,8 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
   u64 turn = 1;
-  hash_map *hm = HM_CREATE(u64, u64);
+  // hash_map *hm = HM_CREATE(u64, u64);
+  hash_map *hm = hm_init(1024 * 1042, sizeof(u64), sizeof(u64));
   char *start_nums = argv[1];
   u64 num = 0;
   for (u32 i = 0; i < strlen(start_nums); ++i) {
@@ -39,6 +40,20 @@ int main(int argc, char *argv[]) {
   }
 
   printf("Part 1: %lu\n", num);
+
+  while (turn < 30000000) {
+    kv_entry entry = hm_get_or_put(hm, &num);
+    if (!entry.found_existing) {
+      num = 0;
+    } else {
+      u64 last_turn = *(u64 *)entry.value_ptr;
+      num = turn - last_turn;
+    }
+    *(u64 *)entry.value_ptr = turn;
+    turn++;
+  }
+
+  printf("Part 2: %lu\n", num);
 
   hm_deinit(hm);
 
