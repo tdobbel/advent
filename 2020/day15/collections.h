4217fb9c8d9880d752d1e5331226c50da0af4444
Dobby <thomas.dobbelaere@uclouvain.be>
2026-03-24 22:44:15 +0100

    implement wyhash directly into collections.h

--- 2020/day15/collections.h
+++ 2020/day15/collections.h
@@ -1,12 +1,14 @@
 #ifndef _COLLECTIONS_H_
 #define _COLLECTIONS_H_
 
-#include "rapidhash.h"
 #include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define uint128_t __uint128_t
+
+typedef uint128_t u128;
 typedef uint64_t u64;
 typedef uint32_t u32;
 typedef int32_t i32;
@@ -26,6 +28,47 @@ vector *vector_create(u64 elem_size);
 void vector_free(vector *vec);
 void *vector_get(vector *vec, u64 index);
 void *vector_append_get(vector *vec);
+
+#define READ4(a) (u64)(*(u32 *)(a))
+
+const u64 secret[4] = {
+    0xa0761d6478bd642f,
+    0xe7037ed1a0b428db,
+    0x8ebc6af09c88c6e3,
+    0x589965cc75374cc3,
+};
+
+typedef struct {
+  u64 a, b;
+  u64 state[3];
+  u64 total_len;
+} Wyhash;
+
+static inline void mum(u64 *a, u64 *b) {
+  u128 r = *a;
+  r *= *b;
+  *a = (u64)r;
+  *b = (u64)(r >> 64);
+}
+
+static inline u64 mix(u64 a_, u64 b_) {
+  u64 a = a_;
+  u64 b = b_;
+  mum(&a, &b);
+  return a ^ b;
+}
+
+static inline void wy_round(Wyhash *self, const u8 *input);
+Wyhash wyhash_init(u64 seed);
+static inline void wyhash_small_key(Wyhash *self, const u8 *input,
+                                    u64 input_len);
+static inline void final0(Wyhash *self) {
+  self->state[0] ^= self->state[1] ^ self->state[2];
+}
+static inline void final1(Wyhash *self, const u8 *input_lb, u64 input_len,
+                          u64 start_pos);
+static inline u64 final2(Wyhash *self);
+u64 wyhash(const u8 *input, u64 input_len, u64 seed);
 
 typedef struct {
   u64 key_size, value_size;
@@ -118,6 +161,84 @@ void vector_free(vector *vec) {
 
 #ifdef HASHMAP_IMPLEMENTATION
 
+static inline void wy_round(Wyhash *self, const u8 *input) {
+  for (u64 i = 0; i < 3; ++i) {
+    u64 a = *(u64 *)(input + 8 * (2 * i));
+    u64 b = *(u64 *)(input + 8 * (2 * i + 1));
+    self->state[i] = mix(a ^ secret[i + 1], b ^ self->state[i]);
+  }
+}
+
+Wyhash wyhash_init(u64 seed) {
+  Wyhash self = (Wyhash){.total_len = 0};
+  self.state[0] = seed ^ mix(seed ^ secret[0], secret[1]);
+  self.state[1] = self.state[0];
+  self.state[2] = self.state[0];
+  return self;
+}
+
+static inline void wyhash_small_key(Wyhash *self, const u8 *input,
+                                    u64 input_len) {
+  assert(input_len <= 16);
+  if (input_len >= 4) {
+    u64 end = input_len - 4;
+    u64 quarter = (input_len >> 3) << 2;
+    self->a = (READ4(input) << 32) | READ4(input + quarter);
+    self->b = (*(u64 *)(input + end) << 32) | *(u64 *)(input + end - quarter);
+  } else if (input_len > 0) {
+    self->a = ((u64)input[0] << 16) | ((u64)input[input_len >> 1] << 8) |
+              (u64)input[input_len - 1];
+    self->b = 0;
+  } else {
+    self->a = 0;
+    self->b = 0;
+  }
+}
+
+static inline void final1(Wyhash *self, const u8 *input_lb, u64 input_len,
+                          u64 start_pos) {
+  assert(input_len >= 16);
+  assert(input_len - start_pos <= 48);
+  const u8 *input = input_lb + start_pos;
+  u64 len = input_len - start_pos;
+
+  u64 i = 0;
+  while (i + 16 < len) {
+    self->state[0] = mix((*(u64 *)(input + i)) ^ secret[1],
+                         (*(u64 *)(input + i + 8)) ^ self->state[0]);
+    i += 16;
+  }
+  self->a = *(u64 *)(input_lb + input_len - 16);
+  self->b = *(u64 *)(input_lb + input_len - 8);
+}
+
+static inline u64 final2(Wyhash *self) {
+  self->a ^= secret[1];
+  self->b ^= self->state[0];
+  mum(&self->a, &self->b);
+  return mix(self->a ^ secret[0] ^ self->total_len, self->b ^ secret[1]);
+}
+
+u64 wyhash(const u8 *input, u64 input_len, u64 seed) {
+  Wyhash self = wyhash_init(seed);
+  if (input_len <= 16) {
+    wyhash_small_key(&self, input, input_len);
+  } else {
+    u64 i = 0;
+    if (input_len >= 48) {
+      while (i + 48 < input_len) {
+        wy_round(&self, input);
+        i += 48;
+      }
+      final0(&self);
+    }
+    final1(&self, input, input_len, i);
+  }
+
+  self.total_len = input_len;
+  return final2(&self);
+}
+
 #define AUTO_HASHMAP(K, V)                                                     \
   hm_init(BASE_CAPACITY,                                                       \
           (hash_map_context){.key_size = sizeof(K), .value_size = sizeof(V)},  \
@@ -162,7 +283,7 @@ b8 bytes_eql(const hash_map_context ctx, const void *a, const void *b) {
 }
 
 u64 hm_get_index(hash_map *hm, const void *key) {
-  u64 hash = rapidhash(key, hm->ctx.key_size);
+  u64 hash = wyhash(key, hm->ctx.key_size, SEED);
   u64 indx = hash & (hm->capacity - 1);
   u64 limit = hm->capacity;
   while (!hm->isfree[indx] && limit > 0) {
