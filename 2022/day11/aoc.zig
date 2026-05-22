94729668c04d5785b3a9ed5bc8193539fd6845d6
tdobbel <thomas.dobbelaere@uclouvain.be>
2026-05-22 10:56:45 +0200

    starting to play with prime numbers...

--- 2022/day11/aoc.zig
+++ 2022/day11/aoc.zig
@@ -1,16 +1,108 @@
 const std = @import("std");
 
 const Operator = enum { Sum, Prod };
+const Number = std.hash_map.AutoHashMap(u64, u64);
+const PrimeError = error{NotDivisibleError};
+
+pub fn prime_decomposition(allocator: std.mem.Allocator, num: u64) !Number {
+    std.debug.print("Prime decomposition started for {}\n", .{num});
+    var x = num;
+    var div: u64 = 2;
+    var out: Number = .init(allocator);
+    while (x > 1 and div * div <= num) : (div += 1) {
+        if (x % div != 0) continue;
+        x /= div;
+        var p: u64 = 1;
+        while (x % div == 0) {
+            x /= div;
+            p += 1;
+        }
+        try out.put(div, p);
+    }
+    if (x > 1) {
+        try out.put(x, 1);
+    }
+    std.debug.print("Prime decomposition ended\n", .{});
+    return out;
+}
+
+pub fn prime_product(num: *Number, factor: Number) !void {
+    var iter = factor.iterator();
+    while (iter.next()) |entry| {
+        const key = entry.key_ptr.*;
+        const value = entry.value_ptr.*;
+        if (num.contains(key)) {
+            try num.put(key, value + num.get(key).?);
+        } else {
+            try num.put(key, value);
+        }
+    }
+}
+
+pub fn prime_divide(num: *Number, divider: Number) !void {
+    var iter = divider.iterator();
+    while (iter.next()) |div_entry| {
+        const prime_factor = div_entry.key_ptr.*;
+        const p1 = div_entry.value_ptr.*;
+        const p2 = num.get(prime_factor) orelse return PrimeError.NotDivisibleError;
+        if (p2 < p1) {
+            return PrimeError.NotDivisibleError;
+        } else if (p2 == p1) {
+            _ = num.remove(prime_factor);
+        } else {
+            try num.put(prime_factor, p2 - p1);
+        }
+    }
+}
+
+pub fn prime_sum(allocator: std.mem.Allocator, num: *Number, other: Number) !void {
+    var gcd: Number = .init(allocator);
+    var iter = other.iterator();
+    while (iter.next()) |entry| {
+        const key = entry.key_ptr.*;
+        const v1 = entry.value_ptr.*;
+        const v2 = num.get(key) orelse continue;
+        try gcd.put(key, @min(v1, v2));
+    }
+    var other2 = try other.clone();
+    try prime_divide(num, gcd);
+    try prime_divide(&other2, gcd);
+    const n = to_number(num) + to_number(&other2);
+    num.deinit();
+    other2.deinit();
+    other2 = try prime_decomposition(allocator, n);
+    try prime_product(&gcd, other2);
+    other2.deinit();
+    num.* = gcd;
+}
+
+pub fn to_number(num: *const Number) u64 {
+    var res: u64 = 1;
+    var iter = num.iterator();
+    while (iter.next()) |entry| {
+        const prime = entry.key_ptr.*;
+        const exp = entry.value_ptr.*;
+        res *= std.math.pow(u64, prime, exp);
+    }
+    return res;
+}
+
+pub fn divide_by_three(allocator: std.mem.Allocator, num: *Number) !void {
+    const n = to_number(num) / 3;
+    const new_num = try prime_decomposition(allocator, n);
+    num.deinit();
+    num.* = new_num;
+}
 
 const MonkeyOperation = struct {
     operator: Operator,
-    rhs: ?u32,
+    rhs: ?Number,
 
-    pub fn apply(self: *const MonkeyOperation, old: u32) u32 {
-        const rhs = self.rhs orelse old;
+    pub fn apply(self: *const MonkeyOperation, allocator: std.mem.Allocator, lhs: *Number) !void {
+        const rhs = self.rhs orelse lhs.*;
         return switch (self.operator) {
-            .Sum => old + rhs,
-            .Prod => old * rhs,
+            .Sum => try prime_sum(allocator, lhs, rhs),
+            .Prod => try prime_product(lhs, rhs),
         };
     }
 };
@@ -27,22 +119,26 @@ pub fn parse_last_number(line: []const u8) u32 {
 }
 
 const Monkey = struct {
-    worry_levels: std.array_list.Managed(u32) = undefined,
-    divider: u32 = undefined,
+    worry_levels: std.array_list.Managed(Number) = undefined,
+    divider: u64 = undefined,
     monkey_true: usize = undefined,
     monkey_false: usize = undefined,
     operation: MonkeyOperation = undefined,
     counter: u32 = 0,
 
-    pub fn apply_rule(self: *const Monkey, x: u32) usize {
-        return if (x % self.divider == 0) self.monkey_true else self.monkey_false;
+    pub fn apply_rule(self: *const Monkey, x: Number) usize {
+        return if (x.contains(self.divider)) self.monkey_true else self.monkey_false;
     }
 
-    pub fn parse_operator(self: *Monkey, line: []const u8) !void {
+    pub fn parse_operator(self: *Monkey, allocator: std.mem.Allocator, line: []const u8) !void {
         const i = std.mem.indexOf(u8, line, "old").?;
         const op: Operator = if (line[i + 4] == '+') .Sum else .Prod;
         const rhs_str = line[(i + 6)..];
-        const rhs = if (std.mem.eql(u8, rhs_str, "old")) null else try std.fmt.parseInt(u32, rhs_str, 10);
+        var rhs: ?Number = null;
+        if (!std.mem.eql(u8, rhs_str, "old")) {
+            const n = try std.fmt.parseInt(u64, rhs_str, 10);
+            rhs = try prime_decomposition(allocator, n);
+        }
         self.operation = MonkeyOperation{ .operator = op, .rhs = rhs };
     }
 
@@ -50,19 +146,19 @@ const Monkey = struct {
         var i: usize = 0;
         while (line[i] != ':') : (i += 1) {}
         const nums: []const u8 = line[(i + 2)..];
-        self.worry_levels = std.array_list.Managed(u32).init(allocator);
+        self.worry_levels = std.array_list.Managed(Number).init(allocator);
         var split_iter = std.mem.splitSequence(u8, nums, ", ");
         while (split_iter.next()) |item| {
-            const v = try std.fmt.parseInt(u32, item, 10);
-            try self.worry_levels.append(v);
+            const v = try std.fmt.parseInt(u64, item, 10);
+            try self.worry_levels.append(try prime_decomposition(allocator, v));
         }
     }
 
     pub fn clone(self: *const Monkey, allocator: std.mem.Allocator) !Monkey {
         var monkey: Monkey = Monkey{ .divider = self.divider, .monkey_true = self.monkey_true, .monkey_false = self.monkey_false, .operation = self.operation, .counter = 0 };
-        monkey.worry_levels = std.array_list.Managed(u32).init(allocator);
+        monkey.worry_levels = std.array_list.Managed(Number).init(allocator);
         for (self.worry_levels.items) |item| {
-            try monkey.worry_levels.append(item);
+            try monkey.worry_levels.append(try item.clone());
         }
         return monkey;
     }
@@ -71,7 +167,7 @@ const Monkey = struct {
         try switch (flag) {
             0 => return,
             1 => self.parse_worry_levels(allocator, line),
-            2 => self.parse_operator(line),
+            2 => self.parse_operator(allocator, line),
             3 => {
                 self.divider = parse_last_number(line);
             },
@@ -86,16 +182,38 @@ const Monkey = struct {
     }
 };
 
-pub fn next_round(monkeys: []Monkey, divide_by_three: bool) !void {
+pub fn print_prime(num: Number) void {
+    var iter = num.iterator();
+    std.debug.print("(", .{});
+    while (iter.next()) |entry| {
+        std.debug.print("{} => {}, ", .{ entry.key_ptr.*, entry.value_ptr.* });
+    }
+    std.debug.print(")\n", .{});
+}
+
+pub fn next_round(allocator: std.mem.Allocator, monkeys: []Monkey, div_by_3: bool) !void {
     for (0..monkeys.len) |i| {
+        std.debug.print("Monkey {}\n", .{i});
         var monkey = &monkeys[i];
-        while (monkey.worry_levels.pop()) |old| {
-            var w = monkey.operation.apply(old);
-            if (divide_by_three) {
-                w /= 3;
+        while (monkey.worry_levels.pop()) |item| {
+            var wlvl = item;
+            std.debug.print("  Worry level is ", .{});
+            print_prime(wlvl);
+            switch (monkey.operation.operator) {
+                .Sum => std.debug.print("  sum with", .{}),
+                .Prod => std.debug.print("  product with", .{}),
             }
-            const indx = monkey.apply_rule(w);
-            try monkeys[indx].worry_levels.append(w);
+            if (monkey.operation.rhs) |num| {
+                print_prime(num);
+            } else {
+                std.debug.print("self\n", .{});
+            }
+            try monkey.operation.apply(allocator, &wlvl);
+            if (div_by_3) {
+                try divide_by_three(allocator, &wlvl);
+            }
+            const indx = monkey.apply_rule(wlvl);
+            try monkeys[indx].worry_levels.append(wlvl);
             monkey.counter += 1;
         }
     }
@@ -114,7 +232,7 @@ pub fn monkey_business(allocator: std.mem.Allocator, monkeys: []const Monkey) !u
 
 pub fn print_monkeys(monkeys: []const Monkey) void {
     for (monkeys, 0..) |monkey, i| {
-        std.debug.print("Monkey {}: {any}\n", .{ i, monkey.worry_levels.items });
+        std.debug.print("Monkey {}\n",.{ i, monkey.worry_levels.items });
     }
 }
 
@@ -153,14 +271,13 @@ pub fn main() !void {
         monkeys2[i] = try m.clone(allocator);
     }
     for (0..20) |_| {
-        try next_round(monkeys, true);
+        try next_round(allocator, monkeys, true);
     }
+    std.debug.print("Part 1: {}\n", .{try monkey_business(allocator, monkeys)});
 
-    std.debug.print("Part 1: {}", .{try monkey_business(allocator, monkeys)});
 
     for (0..20) |_| {
-        try next_round(monkeys2, false);
+        try next_round(allocator, monkeys2, false);
     }
-
-    std.debug.print("Part 2: {}", .{try monkey_business(allocator, monkeys2)});
+    // std.debug.print("Part 2: {}", .{try monkey_business(allocator, monkeys2)});
 }
