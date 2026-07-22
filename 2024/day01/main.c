bb624ee978329ab653e0d73f1d27dd8b390f5258
tdobbel <thomas.dobbelaere@uclouvain.be>
2026-07-22 17:02:28 +0200

    Use custom C lib

--- 2024/day01/main.c
+++ 2024/day01/main.c
@@ -1,60 +1,67 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int compare(const void *a, const void *b) {
-    return (*(int *) a - *(int *) b);
-}
+#define STRING_IMPLEMENTATION
+#include "string8.h"
 
-int getScore(int *arr, int value, size_t n) {
-    int cnt = 0;
-    for (size_t i = 0; i < n; ++i) {
-        cnt += arr[i] == value ? 1 : 0;
-    }
-    return cnt*value;
+#define VECTOR_IMPLEMENTATION
+#include "vector.h"
+
+int compare_fn(const void *a, const void *b) { return (*(u32 *)a - *(u32 *)b); }
+
+int get_score(vector *vec, u32 value) {
+  u32 cntr = 0;
+  u32 *nums = (u32 *)vec->data;
+  for (u64 i = 0; i < vec->size; ++i) {
+    cntr += nums[i] == value ? 1 : 0;
+  }
+  return cntr * value;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Filename required");
-        return 1;
-    }
+  if (argc != 2) {
+    fprintf(stderr, "Missing input file");
+    return EXIT_FAILURE;
+  }
 
-    const size_t MAX_LINE_NUMBER = 1000;
-    const size_t MAX_LINE_LENGTH = 256;
+  string8 file = {0};
+  str_read_file(NULL, &file, argv[1]);
+  vector *line_vec = VEC_CREATE(string8);
+  split(line_vec, file, STR8_LIT("\n"));
+  string8 *lines = (string8 *)line_vec->data;
 
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
-        printf("File '%s' not found \n", argv[1]);
-        return 1;
-    }
+  vector *left_vec = VEC_CREATE(u32);
+  vector *right_vec = VEC_CREATE(u32);
 
-    int *left = malloc(MAX_LINE_NUMBER * sizeof(int));
-    int *right = malloc(MAX_LINE_NUMBER * sizeof(int));
+  vector *splitted_vec = VEC_CREATE(string8);
+  u64 v;
+  for (u64 i = 0; i < line_vec->size && lines[i].size > 0; ++i) {
+    split_whitespace(splitted_vec, lines[i]);
+    string8 *splitted = (string8 *)splitted_vec->data;
+    str_parse_unsigned(&v, splitted[0]);
+    VEC_PUSH(left_vec, u32, (u32)v);
+    str_parse_unsigned(&v, splitted[1]);
+    VEC_PUSH(right_vec, u32, (u32)v);
+  }
+  vector_free(splitted_vec);
+  vector_sort(left_vec, compare_fn);
+  vector_sort(right_vec, compare_fn);
 
-    char line[MAX_LINE_LENGTH];
-    size_t count = 0;
-    while (fgets(line, MAX_LINE_LENGTH, file)) {
-        sscanf(line, "%d   %d", &left[count], &right[count]);
-        count++;
-    }
-    fclose(file);
+  u32 part1 = 0, part2 = 0;
+  u32 *right = (u32 *)right_vec->data;
+  u32 *left = (u32 *)left_vec->data;
 
-    left = realloc(left, count * sizeof(int));
-    qsort(left, count, sizeof(int), compare);
-    right = realloc(right, count * sizeof(int));
-    qsort(right, count, sizeof(int), compare);
+  for (u64 i = 0; i < right_vec->size; ++i) {
+    part1 += right[i] > left[i] ? right[i] - left[i] : left[i] - right[i];
+    part2 += get_score(right_vec, left[i]);
+  }
 
-    int part1 = 0;
-    int part2 = 0;
-    for (int i = 0; i < count; ++i) {
-        part1 += right[i] > left[i] ? right[i] - left[i] : left[i] - right[i];
-        part2 += getScore(right, left[i], count);
-    }
+  printf("Part 1: %u\n", part1);
+  printf("Part 2: %u\n", part2);
 
-    printf("Part 1: %d\n", part1);
-    printf("Part 2: %d\n", part2);
+  vector_free(left_vec);
+  vector_free(right_vec);
+  free(file.str);
 
-    free(left);
-    free(right);
-    return 0;
+  return EXIT_SUCCESS;
 }
